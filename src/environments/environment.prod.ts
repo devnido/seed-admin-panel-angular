7d22158e6d69485f64c71522d8e48f6c07cfcc1e
devnido <luis.nido@hotmail.com>
2020-03-12 06:27:32 -0300

    asda

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+    production: true,
+    baseUrlAuthApi: 'https://auth-demo.devnido.io/api/v1',
+    baseUrlResourcesApi: 'https://api-demo.devnido.io/api/v1',
+    siteApiKeyCaptcha: '6LdvruAUAAAAAFrOk2OVrJNuwb0eXlVujpuUTj0M'
 };
